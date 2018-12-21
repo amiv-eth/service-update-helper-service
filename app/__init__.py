57c37a91ba34dd6ead99b87015ef34752f1469af
Sandro Lutz <code@temparus.ch>
2018-12-21 17:44:15 +0100

    Minor bug fixes

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,10 @@
 from flask import Flask, request, abort
-from functools import wraps
 import docker
 
 
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
 client = docker.from_env()
-
-
-def verify_authorization(func):
-  @wraps(func)
-  def wrapper(*args, **kwargs):
-    token = request.headers.get('Authorization', '').strip()
-    if (' ' in token):  # Remove keywords, e.g. "Token (...)" or "Bearer (...)"
-      token = token.split(' ')[1]
-
-    if token in app.config['TOKEN']:
-      return func(*args, **kwargs)
-    return abort(401)
-  return wrapper
 
 
 @app.errorhandler(401)
@@ -36,18 +22,22 @@ def error_internal(e):
   return 'Could not update the requested service!'
 
 
-@verify_authorization
 @app.route('/service/<name>/update')
 def service_update(name):
+  # Check authorization header first
+  token = request.headers.get('Authorization', '').strip()
+  if (' ' in token):  # Remove keywords, e.g. "Token (...)" or "Bearer (...)"
+    token = token.split(' ')[1]
+
+  if token in app.config['TOKENS']:
+    return func(*args, **kwargs)
+  return abort(401)
+
+  # Try to update the requested service
   try:
     service = client.services.get(name)
     if service.force_update():
       return "Service successfully updated."
-    print('Updating service failed', flush=True)
     abort(500)
   except docker.errors.NotFound:
     abort(404)
-  except e:
-    print('Updating service failed', flush=True)
-    print(e, flush=True)
-    abort(500)
